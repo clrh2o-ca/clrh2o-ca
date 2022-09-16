1970f8df7b547c7815354175eab2ea82a7910831
clrh2o-ca <gclrh2o@gmail.com>
2022-09-15 21:30:45 -0600

    changed host

--- map.js
+++ map.js
@@ -65,7 +65,7 @@ const sheetID = '72900543';
 const sheetKey = 'AIzaSyD6Ic-Sx4PUXKdUweNhjZEi7hzKGJ3QGGU';
 const clientID = '961472646552-8vvhsv52p1fd6256k3k5vdmbnt349i7s.apps.googleusercontent.com';
 const clientSecret = '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';
-const host = "localhost:6969";
+const host = "clrh2o-ca.github.io";
 
 let wells = [];
 let map;
